62466c09031df1f3251f1220b15abc954025ecd8
Jesús García González <jgrcglez@gmail.com>
2022-06-07 15:36:45 -0400

    fooling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>
-          <button className="btn btn-outline-secondary btn-lg">Hola</button>
-        </p>
-      </header>
-    </div>
-  );
+
+    return (
+        <Router>
+            <div>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="about" element={<About/>}/>
+                    <Route path="*" element={<NotFound/>}/>
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <main className="w-100">
+            <div className="row">
+                <img src={"static/1411081.png"} className="img-thumbnail" alt="logo" />
+            </div>
+        </main>
+    );
+}
+
+export default Home;
