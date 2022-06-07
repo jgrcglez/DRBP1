0d42eb6321b225a90edd45466a5984beb60062ff
Jesus <jgrcglez@gmail.com>
2022-06-06 20:22:35 -0400

    Added some more stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React<i className="fa-solid fa-user"></i>
         </a>
       </header>
     </div>
